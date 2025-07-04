63dbc0aa473dc5c9fc2a57e3f4b588af6c45803c
Sultanov Ilyas <SultanovIlyas0@gmail.com>
2025-07-04 11:21:29 +0500

    fead: Connection api for use translate

---
+++ src/connect_api/connect_api.py
@@ -0,0 +1,21 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+import requests
+
+API1_URL = "http://34.9.223.19:8000/translate/ru-kk/"
+
+
+async def translate_text(text: str) -> str:
+    """Функция для отправки текста на перевод через API1"""
+    try:
+        response = requests.post(
+            API1_URL,
+            json={"text": text, "target_language": "ru"}
+        )
+        response.raise_for_status()
+        return response.json().get("translated_text", text)
+    except Exception as e:
+        print(f"Translation error: {e}")
+        return text
